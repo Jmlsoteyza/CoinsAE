e459c98cfbc06b1b0571e4a77e1ec30a0591441d
Your Name <Jmlsoteyza.dev@gmail.com>
2023-03-28 12:21:41 +0400

    yes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -51,14 +51,33 @@ const Footer = () => {
             </a>
           </div>
           <div className="sb__footer-links_div">
-          <h4>Coming soon on</h4>
-          <div className="socialmedia">
-          <p><img /></p>
-          <p><img /></p>
-          <p><img /></p>
-          <p><img /></p>
+            <h4>Coming soon on</h4>
+            <div className="socialmedia">
+              <p>
+                <img />
+              </p>
+              <p>
+                <img />
+              </p>
+              <p>
+                <img />
+              </p>
+            </div>
           </div>
+        </div>
+
+        <hr></hr>
+
+        <div className="sb__footer-below">
+          <div className="sb__footer-copyright">
+            <p>@{new Date().getFullYear()} CodeInn. All right reserved.</p>
           </div>
+        </div>
+        <div className="sb__footer-below-links">
+        <a href="/terms"><div><p>Terms & Conditions</p></div></a>
+        <a href="/terms"><div><p>Privacy</p></div></a>
+        <a href="/terms"><div><p>Security</p></div></a>
+        <a href="/terms"><div><p>Cookei Declaration</p></div></a>
         </div>
       </div>
     </div>
